367865d1a09fd2fa60f8d20de76482c31e5b4ec8
Enrique Gonzalez Montes Junior <twd00026@gmail.com>
2024-03-10 22:14:56 -0300

    routes modifications methods add

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,6 +1,7 @@
 from http.server import BaseHTTPRequestHandler
 import json
 from application.user_serviceImpl import UserServiceImpl
+import cgi
 
 class MyServer(BaseHTTPRequestHandler):
     def _set_headers(self, status_code=200):
@@ -8,10 +9,21 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-    def _parse_post_data(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        return json.loads(post_data.decode())
+    def _parse_request_data(self):
+            # Verificar el método de la solicitud para determinar cómo analizar los datos
+            if self.command == 'POST' or self.command == 'PUT':
+                form = cgi.FieldStorage(
+                    fp=self.rfile,
+                    headers=self.headers,
+                    environ={'REQUEST_METHOD': self.command}
+                )
+                request_data = {}
+                for field in form.keys():
+                    request_data[field] = form[field].value
+                return request_data
+            else:
+                # Manejar otros métodos de solicitud aquí si es necesario
+                return {}
 
     def do_GET(self):
         if self.path == '/users':
@@ -28,4 +40,38 @@ class MyServer(BaseHTTPRequestHandler):
             else:
                 self._set_headers(status_code=404)
                 self.wfile.write(b'User not found')
+
+    def do_POST(self):
+        if self.path == '/users':
+            self._set_headers()
+            user_data = self._parse_request_data()
+            user = UserServiceImpl().create_user(user_data)
+            if user:
+                self._set_headers(status_code=201)
+                self.wfile.write(json.dumps(user).encode())
+            else:
+                self._set_headers(status_code=400)
+                self.wfile.write(b'Failed to create user')
+
+    def do_PUT(self):
+        if self.path.startswith('/users/'):
+            user_id = self.path.split('/')[-1]
+            user_data = self._parse_request_data()
+            success = UserServiceImpl().update_user(user_id, user_data)
+            if success:
+                self._set_headers(status_code=204)
+            else:
+                self._set_headers(status_code=404)
+                self.wfile.write(b'User not found')
+
+    def do_DELETE(self):
+        if self.path.startswith('/users/'):
+            user_id = self.path.split('/')[-1]
+            success = UserServiceImpl().delete_user(user_id)
+            if success:
+                self._set_headers(status_code=204)
+            else:
+                self._set_headers(status_code=404)
+                self.wfile.write(b'User not found')
+
 
